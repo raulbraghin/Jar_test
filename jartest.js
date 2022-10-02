80bc23a1cdf61c76e9be80309a10bc3c738bad83
raulbraghin <raulbraghin@hotmail.com>
2022-10-02 09:09:09 -0300

    x

--- jartest.js
+++ jartest.js
@@ -1,6 +1,31 @@
 function SalvaTipoETA() {
 
     var tipoEta = document.getElementsByName('selecao')
+
+    var tipoetaselecionada = ''
+
+    if (tipoEta[0].checked){
+    
+        tipoetaselecionada = 'Modular'
+        alert('Seleção ' + tipoetaselecionada)
+    }
+    else if (tipoEta[1].checked){
+
+        tipoetaselecionada = 'Torrezan'
+        alert('Seleção ' + tipoetaselecionada)
+
+    }
+    else if (tipoEta[2].checked){
+
+        tipoetaselecionada = 'Tipo não disponível'
+        alert('Seleção ' + tipoetaselecionada)
+
+    }
+    else {
+
+        tipoetaselecionada = 'Nenhuma ETA Selecionada'
+        alert('Seleção ' + tipoetaselecionada)
+    }
     
 
 }
